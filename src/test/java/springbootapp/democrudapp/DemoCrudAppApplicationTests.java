ef20c936ad036f384e05f7695f7cbccbbcfc4758
Andrei-Zmn <andrei.zeman@yahoo.com>
2022-11-23 18:41:06 +0200

    Initial commit

---
+++ src/test/java/springbootapp/democrudapp/DemoCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootapp.democrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
